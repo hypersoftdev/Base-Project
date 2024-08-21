639e83640322edb3ea2a504f76e9eec60b061cc1
Sohaib Ahmed <sohaibahmed.hypersoft@gmail.com>
2024-08-21 17:27:05 +0500

    Base Project

--- app/src/main/java/com/hypersoft/baseproject/helpers/interfaces/OnLanguageItemClickListener.kt
+++
@@ -1,7 +0,0 @@
-package com.hypersoft.baseproject.helpers.interfaces
-
-import com.hypersoft.baseproject.helpers.models.LanguageItem
-
-interface OnLanguageItemClickListener {
-    fun onItemClick(languageItem: LanguageItem)
-}
