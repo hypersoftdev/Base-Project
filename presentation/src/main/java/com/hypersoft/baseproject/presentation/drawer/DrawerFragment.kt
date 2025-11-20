ccbd8da06fa40fd1c23d9fdc40a4b141d355490b
desk11 <lead1@hypersoft.tech>
2025-11-20 09:52:56 +0500

    Base Project (MVI) - Drawer Screen

---
+++ presentation/src/main/java/com/hypersoft/baseproject/presentation/drawer/DrawerFragment.kt
@@ -0,0 +1,11 @@
+package com.hypersoft.baseproject.presentation.drawer
+
+import com.hypersoft.baseproject.core.base.fragment.BaseFragment
+import com.hypersoft.baseproject.presentation.databinding.FragmentDrawerBinding
+
+class DrawerFragment : BaseFragment<FragmentDrawerBinding>(FragmentDrawerBinding::inflate) {
+
+    override fun onViewCreated() {
+
+    }
+}
